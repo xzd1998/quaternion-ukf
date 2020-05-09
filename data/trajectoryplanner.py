bug: normalizing vectors

data/trajectoryplanner.py
@@ -46,10 +46,10 @@ class TrajectoryPlanner:
 
 class SimplePlanner(TrajectoryPlanner):
 
-    def __init__(self, noise_stddev=0.01, drift_stddev = 0.001):
+    def __init__(self, noise_stddev=0, drift_stddev=0):
         duration = 20
         dt = 0.01
-        da = 1 / 4000  # 3450
+        da = 1 / 3450 * np.array([1, 1, 0])
         super().__init__(
             duration,
             dt,

data/utilities.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.constants import g
 
 
-def rots_to_angles(rots):
+def rots_to_angles_zyx(rots):
     roll = np.arctan2(rots[2, 1], rots[2, 2])
     pitch = np.arctan2(-rots[2, 0], np.sqrt(np.square(rots[2, 1]) + np.square(rots[2, 2])))
     yaw = np.arctan2(rots[1, 0], rots[0, 0])
@@ -11,8 +11,16 @@ def rots_to_angles(rots):
     return roll, pitch, yaw
 
 
+def rots_to_angles_xyz(rots):
+    roll = np.arctan2(-rots[1, 2], rots[2, 2])
+    pitch = np.arctan2(rots[0, 2], np.sqrt(np.square(rots[1, 2]) + np.square(rots[2, 2])))
+    yaw = np.arctan2(-rots[0, 1], rots[0, 0])
+
+    return roll, pitch, yaw
+
+
 def rots_to_vels(rots, ts):
-    roll, pitch, yaw = rots_to_angles(rots)
+    roll, pitch, yaw = rots_to_angles_zyx(rots)
     make_angles_continuous([roll, pitch, yaw])
     dts = np.diff(ts)
     dr, dp, dy = (np.diff(roll) / dts, np.diff(pitch) / dts, np.diff(yaw) / dts)
@@ -43,6 +51,37 @@ def moving_average(data, n=9):
     for i in range(data.shape[0]):
         averaged[i] = np.convolve(data[i], np.ones(n) / n, "same")
     return averaged
+
+
+def vectors_to_rots(raw):
+    vecs = np.copy(raw)
+    if len(vecs.shape) == 1:
+        vecs = vecs.reshape(-1, 1)
+    rots = np.zeros((3, 3, vecs.shape[-1]))
+    k = np.zeros((3, vecs.shape[-1]))
+    theta = np.linalg.norm(vecs, axis=0)
+    indexer = theta > 0
+
+    k[:, indexer] = vecs[:, indexer] / theta[indexer]
+    kx = k[0]
+    ky = k[1]
+    kz = k[2]
+
+    c_th = np.cos(theta)
+    v_th = 1 - np.cos(theta)
+    s_th = np.sin(theta)
+
+    rots[0, 0] = np.square(kx) * v_th + c_th
+    rots[1, 0] = kx * ky * v_th + kz * s_th
+    rots[2, 0] = kx * kz * v_th - ky * s_th
+    rots[0, 1] = kx * ky * v_th - kz * s_th
+    rots[1, 1] = np.square(ky) * v_th + c_th
+    rots[2, 1] = ky * kz * v_th + kx * s_th
+    rots[0, 2] = kx * kz * v_th + ky * s_th
+    rots[1, 2] = ky * kz * v_th - kx * s_th
+    rots[2, 2] = np.square(kz) * v_th + c_th
+
+    return rots
 
 
 def angles_to_rots_zyx(roll, pitch, yaw):
@@ -102,8 +141,8 @@ if __name__ == "__main__":
     rpy = np.random.randn(3, 1)
     print(rpy)
     rot = angles_to_rots_zyx(rpy[0], rpy[1], rpy[2])
-    r, p, y = rots_to_angles(rot)
+    r, p, y = rots_to_angles_zyx(rot)
     rpy_again = np.array([r, p, y]).reshape(3, 1)
-    print(rots_to_angles(rot))
+    print(rots_to_angles_zyx(rot))
     print(np.abs(rpy - rpy_again) < .001)
 

imufilter.py
@@ -18,6 +18,14 @@ class ImuFilter(ABC):
         """Applies chosen filtering technique to estimate rotation matrices"""
         pass
 
+    def _integrate_vel(self):
+        angles = np.zeros((3, self.num_data))
+        dts = np.diff(self.ts_imu)
+        da_dts = (self.vel_data[:, :-1] + self.vel_data[:, 1:]) * dts / 2
+        print(da_dts.shape)
+        angles[:, 1:] = np.cumsum(da_dts, axis=1)
+        return angles
+
     @property
     def num_data(self):
         """Length of imu data array"""
@@ -36,7 +44,7 @@ class ImuFilter(ABC):
     @lazy
     def angles(self):
         """Tuple of estimated roll, pitch, and yaw angles"""
-        return utilities.rots_to_angles(self.rots)
+        return utilities.rots_to_angles_zyx(self.rots)
 
     @lazy
     def acc_data(self):
@@ -60,8 +68,8 @@ class ImuFilter(ABC):
 
         labels = ["Roll", "Pitch", "Yaw"]
 
-        a = utilities.rots_to_angles(rots_est_copy)
-        angs = utilities.rots_to_angles(rots_truth_copy)
+        a = utilities.rots_to_angles_zyx(rots_est_copy)
+        angs = utilities.rots_to_angles_zyx(rots_truth_copy)
 
         for i in range(3):
             plt.figure(i)

quaternions.py
@@ -49,7 +49,7 @@ class Quaternions:
             err_rot = err_quat.to_vectors()
             err_rot_mean = np.mean(err_rot, axis=1)  # Equation 54
 
-            err_quat_mean = Quaternions.from_vector(err_rot_mean)
+            err_quat_mean = Quaternions.from_vectors(err_rot_mean)
             q_mean = err_quat_mean.q_multiply(q_mean)  # Equation 55
 
             err = np.linalg.norm(err_rot_mean)
@@ -97,7 +97,7 @@ class Quaternions:
             v[..., not_ind] = theta[not_ind].astype(float) / np.sin(theta[not_ind] * .5) * self.array[1:, not_ind]
             return v
         v = theta.astype(float) / np.sin(theta * .5) * self.array[1:]
-        return v.astype(float) / np.linalg.norm(v, axis=0)
+        return v.astype(float)  # / np.linalg.norm(v, axis=0)
 
     def inverse(self):
         """
@@ -119,7 +119,7 @@ class Quaternions:
         return R
 
     @staticmethod
-    def from_vector(raw):
+    def from_vectors(raw):
         """
         Converts vector or array of vectors to an array of quaternions
         :param raw: vector to convert

ukf.py
@@ -1,8 +1,7 @@
 import argparse
 
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.io import loadmat
+from scipy.constants import g
 
 from data import utilities
 from data.datamaker import DataMaker
@@ -29,10 +28,13 @@ class Ukf(ImuFilter):
         self.P = np.zeros((Ukf.N_DIM, Ukf.N_DIM, self.mu.shape[-1]))
         self.P[..., 0] = np.identity(Ukf.N_DIM) * .5
 
+        self.t = 0
+
     def filter_data(self):
 
         self.imu_data[3:, :] = self.imu_data[3:, :] - ((np.mean(self.imu_data[3:, :50], axis=1) +
                                                         np.mean(self.imu_data[3:, -50:], axis=1)) / 2).reshape(3, 1)
+        # Normalized because the gravity vector is also normalized
         self.imu_data[:3] = self.imu_data[:3] / np.linalg.norm(self.imu_data[:3], axis=0)
 
         bot_rots = np.zeros((3, 3, self.mu.shape[-1]))
@@ -40,6 +42,7 @@ class Ukf(ImuFilter):
 
         for i in range(1, self.mu.shape[-1]):
             dt = self.ts_imu[i] - self.ts_imu[i - 1]
+            self.t += dt
 
             self.mu[:, i], self.P[..., i] = self._filter_next(
                 self.P[..., i - 1],
@@ -61,7 +64,7 @@ class Ukf(ImuFilter):
         W = np.concatenate((np.zeros((n, 1)), W), axis=1)
 
         # Equation 34: Form sigma points based on prior mean and covariance data
-        qW = Quaternions.from_vector(W[:3])
+        qW = Quaternions.from_vectors(W[:3])
         q_last = Quaternions(mu_last[:4])
         q_sigpt = q_last.q_multiply(qW)
 
@@ -70,17 +73,20 @@ class Ukf(ImuFilter):
         w_sigpt = w_last.reshape(-1, 1) * np.ones(wW.shape) + wW
 
         # Equations 9-11: form q_delta
-        ad = np.linalg.norm(w_sigpt, axis=0) * dt
-        ed = np.zeros(w_sigpt.shape)
-        if np.any(ad == 0):
-            ind = ad == 0
-            not_ind = ad != 0
-            ed[:, ind] = np.zeros((3, np.sum(ind)))
-            ed[:, not_ind] = w_sigpt[:, not_ind].astype(float) * dt / ad[not_ind]
-        else:
-            ed = -w_sigpt.astype(float) * dt / ad
-        qd = np.array([np.cos(ad * .5), ed[0] * np.sin(ad * .5), ed[1] * np.sin(ad * .5), ed[2] * np.sin(ad * .5)])
-        qd = Quaternions(qd.astype(float) / np.linalg.norm(qd, axis=0))
+        # TODO problem area
+        wd = w_sigpt * dt
+        qd = Quaternions.from_vectors(wd)
+        # ad = np.linalg.norm(w_sigpt, axis=0) * dt
+        # ed = np.zeros(w_sigpt.shape)
+        # if np.any(ad == 0):
+        #     ind = ad == 0
+        #     not_ind = ad != 0
+        #     ed[:, ind] = np.zeros((3, np.sum(ind)))
+        #     ed[:, not_ind] = w_sigpt[:, not_ind].astype(float) * dt / ad[not_ind]
+        # else:
+        #     ed = -w_sigpt.astype(float) * dt / ad
+        # qd = np.array([np.cos(ad * .5), ed[0] * np.sin(ad * .5), ed[1] * np.sin(ad * .5), ed[2] * np.sin(ad * .5)])
+        # qd = Quaternions(qd.astype(float) / np.linalg.norm(qd, axis=0))
 
         # Equation 22: Apply non-linear function A with process noise of zero
         qY = q_sigpt.q_multiply(qd)
@@ -95,8 +101,14 @@ class Ukf(ImuFilter):
         mu_this_est = np.concatenate((q_mean.array.reshape(-1), w_mean.reshape(-1)))
 
         # Equations 65-67: Transform Y into W', notated as Wp for prime
-        rWp = q_mean.inverse().q_multiply(qs).to_vectors()
-        # rWp = qs.q_multiply(q_mean.inverse()).to_vectors()
+        # TODO problem area
+        # qWp = qs.q_multiply(q_mean.inverse())
+        # rWp = q_mean.inverse().q_multiply(qs).to_vectors()
+        rWp = qs.q_multiply(q_mean.inverse()).to_vectors()
+        # print("---------------------------------")
+        # print("Time: {} seconds".format(self.t))
+        # print("Error quaternion:\n{}".format(qWp))
+        # print("Error vector:\n{}".format(np.round(rWp, 3).T))
         wWp = Y[4:, :] - w_mean.reshape(-1, 1)
         Wp = np.concatenate((rWp, wWp))
 
@@ -129,7 +141,7 @@ class Ukf(ImuFilter):
 
         # Equation 74
         r_this = np.matmul(K, (z_this - z_est).reshape(-1, 1)).reshape(-1)
-        q_this = Quaternions.from_vector(r_this[:3])
+        q_this = Quaternions.from_vectors(r_this[:3])
         w_this = r_this[3:]
 
         # Equation 46

yawintegrator.py
@@ -1,4 +1,5 @@
 import argparse
+import matplotlib.pyplot as plt
 import numpy as np
 
 from data import utilities
@@ -8,22 +9,16 @@ from data.trajectoryplanner import SimplePlanner
 from imufilter import ImuFilter
 
 
-class NoFilter(ImuFilter):
+class YawIntegrator(ImuFilter):
 
     N_DIM = 6
 
     def filter_data(self):
 
-        self.source = source
         roll, pitch = utilities.accs_to_roll_pitch(self.acc_data)
-        yaw = np.zeros(self.num_data)
-        yaw[1:] = self._estimate_yaw()
+        vectors = self._integrate_vel()
+        yaw = utilities.rots_to_angles_zyx(utilities.vectors_to_rots(vectors))[-1]
         self.rots = utilities.angles_to_rots_zyx(roll, pitch, yaw)
-
-    def _estimate_yaw(self):
-        dts = np.diff(self.ts_imu)
-        dy_dts = (self.vel_data[-1, :-1] + self.vel_data[-1, 1:]) * dts / 2
-        return np.cumsum(dy_dts)
 
 
 if __name__ == "__main__":
@@ -38,15 +33,15 @@ if __name__ == "__main__":
     if not num:
         planner = SimplePlanner()
         source = DataMaker(planner)
-        m = np.ones(NoFilter.N_DIM)
-        b = np.zeros(NoFilter.N_DIM)
+        m = np.ones(YawIntegrator.N_DIM)
+        b = np.zeros(YawIntegrator.N_DIM)
     else:
         source = DataStore(dataset_number=num, path_to_data="data")
 
-    f = NoFilter(source)
+    f = YawIntegrator(source)
     f.filter_data()
 
     if not num:
-        utilities.plot_rowwise_data(["z-axis"], ["x", "y", "z"], [source.ts, source.ts], source.angs_vicon, f.angles)
+        utilities.plot_rowwise_data(["z-axis"], ["x", "y", "z"], [source.ts, source.ts], source.angles, f.angles)
     else:
         f.plot_comparison(f.rots, f.ts_imu, source.rots_vicon, source.ts_vicon)