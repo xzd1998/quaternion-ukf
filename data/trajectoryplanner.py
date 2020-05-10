fix: calculation of g

data/trajectoryplanner.py
@@ -16,6 +16,7 @@ class TrajectoryPlanner:
         # if duration % dt != 0:
         #     raise ValueError("Duration {} s not divisible by time increment {} s".format(duration, dt))
 
+        np.random.seed(0)
         self.noise = np.random.randn(self.num_data) * noise_stddev
         self.drift = np.cumsum(np.random.randn(self.num_data) * drift_stddev)
 
@@ -46,7 +47,7 @@ class TrajectoryPlanner:
 
 class SimplePlanner(TrajectoryPlanner):
 
-    def __init__(self, noise_stddev=0, drift_stddev=0):
+    def __init__(self, noise_stddev=0.1, drift_stddev=0.0004):
         duration = 20
         dt = 0.01
         da = 1 / 3450 * np.array([1, 1, 0])
@@ -59,6 +60,27 @@ class SimplePlanner(TrajectoryPlanner):
             TrajectoryPlanner.incrementer(da),
             TrajectoryPlanner.decrementer(da),
             TrajectoryPlanner.incrementer(da)
+        )
+
+
+class RoundTripPlanner(TrajectoryPlanner):
+
+    def __init__(self, noise_stddev=0.1, drift_stddev=0.0004):
+        duration = 30
+        dt = 0.01
+        da = 1 / 2000 * np.array([1, 1, 0])
+        super().__init__(
+            duration,
+            dt,
+            noise_stddev,
+            drift_stddev,
+            ((4, 7), (7, 13), (13, 16), (16, 19), (19, 25), (25, 28)),
+            TrajectoryPlanner.incrementer(da),
+            TrajectoryPlanner.decrementer(da),
+            TrajectoryPlanner.incrementer(da),
+            TrajectoryPlanner.decrementer(da),
+            TrajectoryPlanner.incrementer(da),
+            TrajectoryPlanner.decrementer(da)
         )
 
 

quaternion_integrator.py
@@ -1,11 +1,9 @@
-import argparse
-import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import g
 
 from data import utilities
 from data.datamaker import DataMaker
-from data.datastore import DataStore
-from data.trajectoryplanner import SimplePlanner
+from data.trajectoryplanner import RoundTripPlanner
 from imufilter import ImuFilter
 from quaternions import Quaternions
 
@@ -16,44 +14,39 @@ class QuaternionIntegrator(ImuFilter):
 
     def __init__(self, source):
         super().__init__(source)
+        self.quats = []
+        self.acc_calc = np.zeros((3, self.num_data))
+        self.g_quat = Quaternions([0, 0, 0, 1])
 
     def filter_data(self):
         self.rots = np.zeros((3, 3, self.num_data))
-        q = Quaternions([1, 0, 0, 0])
+        self._store_next(Quaternions([1, 0, 0, 0]))
+
         for i in range(0, self.num_data - 1):
-            self.rots[..., i] = q.to_rotation_matrix()
             dt = self.ts_imu[i + 1] - self.ts_imu[i]
-            q = self._filter_next(q, self.vel_data[:, i], dt)
+            self._filter_next(self.vel_data[:, i], dt)
 
-        self.rots[..., -1] = q.to_rotation_matrix()
-
-    @staticmethod
-    def _filter_next(q, w, dt):
+    def _filter_next(self, w, dt):
         qd = Quaternions.from_vectors(w * dt)
-        return q.q_multiply(qd)
+        q_next = qd.q_multiply(self.quats[-1])
+        self._store_next(q_next)
+
+    def _store_next(self, q_next):
+        i = len(self.quats)
+        self.quats.append(q_next)
+        self.rots[..., i] = q_next.to_rotation_matrix()
+        self.acc_calc[:, i] = q_next.q_multiply(self.g_quat).q_multiply(q_next.inverse()).array[1:]
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-D", "--datanum", required=False, help="Number of data file (1 to 3 inclusive)")
-
-    args = vars(parser.parse_args())
-
-    num = args["datanum"]
-    if not num:
-        planner = SimplePlanner()
-        source = DataMaker(planner)
-        m = np.ones(QuaternionIntegrator.N_DIM)
-        b = np.zeros(QuaternionIntegrator.N_DIM)
-    else:
-        source = DataStore(dataset_number=num, path_to_data="data")
+    planner = RoundTripPlanner()
+    source = DataMaker(planner)
+    m = np.ones(QuaternionIntegrator.N_DIM)
+    b = np.zeros(QuaternionIntegrator.N_DIM)
 
     f = QuaternionIntegrator(source)
     f.filter_data()
 
-    if not num:
-        utilities.plot_rowwise_data(["z-axis"], ["x", "y", "z"], [source.ts, source.ts], source.angles, f.angles)
-    else:
-        f.plot_comparison(f.rots, f.ts_imu, source.rots_vicon, source.ts_vicon)
+    utilities.plot_rowwise_data(["z-axis"], ["x", "y", "z"], [source.ts, source.ts], source.angles, f.angles)
+    # utilities.plot_rowwise_data(["z-axis"], ["x", "y", "z"], [source.ts, source.ts], source.acc_data / g, f.acc_calc)

quaternions.py
@@ -44,7 +44,7 @@ class Quaternions:
 
         for _ in range(iterations):
             err_quat = self.q_multiply(q_mean.inverse())  # Equation 52
-            # err_quat = Quaternions(err_quat.array * np.sign(err_quat.array[0]))
+            err_quat = Quaternions(err_quat.array * np.sign(err_quat.array[0]))
 
             err_rot = err_quat.to_vectors()
             err_rot_mean = np.mean(err_rot, axis=1)  # Equation 54
@@ -97,7 +97,7 @@ class Quaternions:
             v[..., not_ind] = theta[not_ind].astype(float) / np.sin(theta[not_ind] * .5) * self.array[1:, not_ind]
             return v
         v = theta.astype(float) / np.sin(theta * .5) * self.array[1:]
-        return v.astype(float)  # / np.linalg.norm(v, axis=0)
+        return v.astype(float) / np.linalg.norm(v, axis=0)
 
     def inverse(self):
         """

roll_pitch_calculator.py
@@ -9,15 +9,14 @@ from data.trajectoryplanner import SimplePlanner
 from imufilter import ImuFilter
 
 
-class YawIntegrator(ImuFilter):
+class RollPitchCalculator(ImuFilter):
 
     N_DIM = 6
 
     def filter_data(self):
 
         roll, pitch = utilities.accs_to_roll_pitch(self.acc_data)
-        vectors = self._integrate_vel()
-        yaw = utilities.rots_to_angles_zyx(utilities.vectors_to_rots(vectors))[-1]
+        yaw = np.zeros(roll.shape[0])
         self.rots = utilities.angles_to_rots_zyx(roll, pitch, yaw)
 
 
@@ -33,12 +32,12 @@ if __name__ == "__main__":
     if not num:
         planner = SimplePlanner()
         source = DataMaker(planner)
-        m = np.ones(YawIntegrator.N_DIM)
-        b = np.zeros(YawIntegrator.N_DIM)
+        m = np.ones(RollPitchCalculator.N_DIM)
+        b = np.zeros(RollPitchCalculator.N_DIM)
     else:
         source = DataStore(dataset_number=num, path_to_data="data")
 
-    f = YawIntegrator(source)
+    f = RollPitchCalculator(source)
     f.filter_data()
 
     if not num:

ukf.py
@@ -6,7 +6,7 @@ from scipy.constants import g
 from data import utilities
 from data.datamaker import DataMaker
 from data.datastore import DataStore
-from data.trajectoryplanner import SimplePlanner, StationaryPlanner
+from data.trajectoryplanner import SimplePlanner, StationaryPlanner, RoundTripPlanner
 from imufilter import ImuFilter
 from quaternions import Quaternions
 
@@ -54,14 +54,22 @@ class Ukf(ImuFilter):
 
         self.rots = bot_rots
 
+    def _debug_print(self, t_min, t_max, *contents):
+        if t_min <= self.t <= t_max:
+            print("Time {} seconds".format(self.t))
+            for content in contents:
+                print(content)
+
     def _filter_next(self, P_last, mu_last, z_this, dt):
 
         # 2n sigma points
         n = P_last.shape[0]
         m = np.sqrt(n)
-        S = np.linalg.cholesky(P_last + self.Q)
+        S = np.linalg.cholesky(P_last)  # + self.Q)
         W = np.concatenate((m * S, -m * S), axis=1)
         W = np.concatenate((np.zeros((n, 1)), W), axis=1)
+
+        # self._debug_print(11.7, 12, np.round(P_last, 2), np.round(S, 2))
 
         # Equation 34: Form sigma points based on prior mean and covariance data
         qW = Quaternions.from_vectors(W[:3])
@@ -73,23 +81,12 @@ class Ukf(ImuFilter):
         w_sigpt = w_last.reshape(-1, 1) * np.ones(wW.shape) + wW
 
         # Equations 9-11: form q_delta
-        # TODO problem area
         wd = w_sigpt * dt
         qd = Quaternions.from_vectors(wd)
-        # ad = np.linalg.norm(w_sigpt, axis=0) * dt
-        # ed = np.zeros(w_sigpt.shape)
-        # if np.any(ad == 0):
-        #     ind = ad == 0
-        #     not_ind = ad != 0
-        #     ed[:, ind] = np.zeros((3, np.sum(ind)))
-        #     ed[:, not_ind] = w_sigpt[:, not_ind].astype(float) * dt / ad[not_ind]
-        # else:
-        #     ed = -w_sigpt.astype(float) * dt / ad
-        # qd = np.array([np.cos(ad * .5), ed[0] * np.sin(ad * .5), ed[1] * np.sin(ad * .5), ed[2] * np.sin(ad * .5)])
-        # qd = Quaternions(qd.astype(float) / np.linalg.norm(qd, axis=0))
 
         # Equation 22: Apply non-linear function A with process noise of zero
-        qY = q_sigpt.q_multiply(qd)
+        # qY = q_sigpt.q_multiply(qd)
+        qY = qd.q_multiply(q_sigpt)
         Y = np.concatenate((qY.array, w_sigpt))
 
         # Equations 52-55: Use mean-finding algorithm to satisfy Equation 38
@@ -101,26 +98,22 @@ class Ukf(ImuFilter):
         mu_this_est = np.concatenate((q_mean.array.reshape(-1), w_mean.reshape(-1)))
 
         # Equations 65-67: Transform Y into W', notated as Wp for prime
-        # TODO problem area
-        # qWp = qs.q_multiply(q_mean.inverse())
-        # rWp = q_mean.inverse().q_multiply(qs).to_vectors()
-        rWp = qs.q_multiply(q_mean.inverse()).to_vectors()
-        # print("---------------------------------")
-        # print("Time: {} seconds".format(self.t))
-        # print("Error quaternion:\n{}".format(qWp))
-        # print("Error vector:\n{}".format(np.round(rWp, 3).T))
+        rWp = q_mean.inverse().q_multiply(qs).to_vectors()
+        # rWp = qs.q_multiply(q_mean.inverse()).to_vectors()
         wWp = Y[4:, :] - w_mean.reshape(-1, 1)
         Wp = np.concatenate((rWp, wWp))
 
         # Equation 64
         Pk_bar = np.matmul(Wp, Wp.T)
+        Pk_bar /= W.shape[1]
+        Pk_bar += self.Q
 
         # Equation 27 and 40
         g = Quaternions(np.array([0, 0, 0, 1]))
-        gp = qs.inverse().q_multiply(g).q_multiply(qs)
+        # gp = qs.inverse().q_multiply(g).q_multiply(qs)
+        gp = qs.q_multiply(g).q_multiply(qs.inverse())
         Z = np.concatenate((gp.array[1:], Y[4:, :]))
 
-        Pk_bar /= W.shape[1]
 
         # Equation 48
         z_est = np.mean(Z, axis=1)
@@ -147,6 +140,7 @@ class Ukf(ImuFilter):
         # Equation 46
         mu_this = np.zeros(mu_this_est.shape)
         mu_this[:4] = Quaternions(mu_this_est[:4]).q_multiply(q_this).array
+        # mu_this[:4] = q_this.q_multiply(Quaternions(mu_this_est[:4])).array
         mu_this[4:] = mu_this_est[4:] + w_this
 
         # Equation 75:
@@ -172,7 +166,7 @@ if __name__ == "__main__":
 
     num = args["datanum"]
     if not num:
-        planner = SimplePlanner()
+        planner = RoundTripPlanner()
         source = DataMaker(planner)
     else:
         source = DataStore(dataset_number=num, path_to_data="data")
@@ -181,6 +175,6 @@ if __name__ == "__main__":
     f.filter_data()
 
     if not num:
-        utilities.plot_rowwise_data(["z-axis"], ["x", "y", "z"], [source.ts, source.ts], source.angs_vicon, f.angles)
+        utilities.plot_rowwise_data(["z-axis"], ["x", "y", "z"], [source.ts, source.ts], source.angles, f.angles)
     else:
         ImuFilter.plot_comparison(f.rots, f.ts_imu, source.rots_vicon, source.ts_vicon)